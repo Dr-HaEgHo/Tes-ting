b4be30cd7d3b9e84ba6c637b36e1f54776981b3a
Teem1345 <awogbuyitimothy@gmail.com>
2022-02-05 18:25:59 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  content: [
+    './dist/*.html',
+  ],
+  theme: {
+    extend: {
+
+      spacing:{
+        '85%':'85%'
+      },
+
+      backgroundImage:{
+        starry:'url(/dist/img/tehihi)'
+      },
+
+      colors:{
+        darkbg:'#171c23',
+        lightbg:'#a6c1ff',
+        darkred:'#450000',
+
+      },
+
+      fontFamily:{
+        Poppins: 'Poppins, sans-serif',
+      }
+
+
+      
+    },
+
+    container:{
+      center:true,
+      padding: '4rem',
+    },
+    
+  },
+  plugins: [],
+}
